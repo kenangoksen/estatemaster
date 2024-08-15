26df2a84ed506ed97611414939bd0cce8a197f67
kgksn <gksnknn@gmail.com>
2024-08-15 21:18:47 +0300

    deneme

--- estatemaster.client/src/views/front/Home.vue
+++ estatemaster.client/src/views/front/Home.vue
@@ -28,7 +28,7 @@
 											</li>
 											<!--end::Item-->
 											<!--begin::Item-->
-											<li class="breadcrumb-item text-muted">Dashboards</li>
+											<li class="breadcrumb-item text-muted">Dashboardsa</li>
 											<!--end::Item-->
 										</ul>
 										<!--end::Breadcrumb-->
